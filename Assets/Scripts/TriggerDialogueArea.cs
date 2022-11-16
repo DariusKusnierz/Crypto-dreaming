70af177da329893e0f65a183db8bcf7b57405352
DariusKusnierz <72936095+DariusKusnierz@users.noreply.github.com>
2022-11-16 17:46:22 +0100

    Dodano odpychanie gracza w strefie wyzwalania dialogu

--- Assets/Scripts/TriggerDialogueArea.cs
+++ Assets/Scripts/TriggerDialogueArea.cs
@@ -7,12 +7,51 @@ public class TriggerDialogueArea : MonoBehaviour
 {
     [SerializeField] Dialogue dialogueToStart;
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    PlayerMovement.Direction actualDirection;
+    PlayerMovement.Direction targetDirection;
+    private bool isMovingBack = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.name != "Player")
+        if (collision.name != "Player")
             return;
 
         dialogueToStart.StartDialogue();
+
+        SpecifyOutDirection();
+        StartCoroutine(MoveBackCoroutine());
     }
 
+    IEnumerator MoveBackCoroutine()
+    {
+        yield return new WaitForSeconds(0.1f);
+
+        PlayerMovement.instance.enabled = false;
+        isMovingBack = true;
+
+        while (isMovingBack)
+        {
+            PlayerMovement.instance.MoveToDirection(targetDirection);
+            yield return new WaitForSeconds(0.01f);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        isMovingBack = false;
+    }
+
+    void SpecifyOutDirection()
+    {
+        actualDirection = PlayerMovement.instance.playerDirection;
+
+        if (actualDirection == PlayerMovement.Direction.Up)
+            targetDirection = PlayerMovement.Direction.Down;
+        else if (actualDirection == PlayerMovement.Direction.Down)
+            targetDirection = PlayerMovement.Direction.Up;
+        else if (actualDirection == PlayerMovement.Direction.Left)
+            targetDirection = PlayerMovement.Direction.Right;
+        else if (actualDirection == PlayerMovement.Direction.Right)
+            targetDirection = PlayerMovement.Direction.Left;
+    }
 }

4c2d73daf8c5ed96b9560b2e27f631b0fd6ebaef
DariusKusnierz <72936095+DariusKusnierz@users.noreply.github.com>
2023-01-09 15:54:49 +0100

    Dodano panel podpowiedzi

--- Assets/Scripts/Interaction.cs
+++ Assets/Scripts/Interaction.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
 
 [RequireComponent(typeof(Collider2D))]
 public class Interaction : MonoBehaviour
@@ -9,18 +10,8 @@ public class Interaction : MonoBehaviour
     [SerializeField] bool canTalk = false;
     [SerializeField] bool isQuest = false;
     [SerializeField] GameObject answerPanel = null;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+    public UnityEvent onInteracted;
     public void Interact()
     {
         Debug.Log("Interakcja z " + gameObject.name);
@@ -33,5 +24,7 @@ public class Interaction : MonoBehaviour
             answerPanel.GetComponentInChildren<TMP_InputField>().ActivateInputField();
             PlayerMovement.instance.enabled = false;
         }
+
+        onInteracted.Invoke();
     }
 }
--- Assets/Scripts/MessagePanel.cs
+++ Assets/Scripts/MessagePanel.cs
@@ -4,17 +4,25 @@ using UnityEngine;
 
 public class MessagePanel : MonoBehaviour
 {
-    [SerializeField] GameObject messagePanel;
-    [SerializeField] GameObject messageIcon;
+    [SerializeField] List<GameObject> messagePanel;
+    [SerializeField] List<GameObject> messageIcon;
     
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
-            if (!messageIcon.activeSelf)
+            if (!messageIcon[0].activeSelf)
                 return;
 
-            messagePanel.SetActive(!messagePanel.activeSelf);
+            messagePanel[0].SetActive(!messagePanel[0].activeSelf);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            if (!messageIcon[1].activeSelf)
+                return;
+
+            messagePanel[1].SetActive(!messagePanel[1].activeSelf);
         }
     }
 }
